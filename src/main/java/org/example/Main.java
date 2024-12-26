b411fbdf51d1843d4f1f2bd4a7498462d9b8c1a8
Marcus Rangel <marcus.vbrangel@gmail.com>
2024-12-26 15:54:05 -0300

    feat: implementado as operações matemáticas

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,23 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+    public static void main(final String... args) {
+
+        int resultadoSoma = OperacoesMatematicas.soma(1, 4);
+        System.out.println(resultadoSoma);
+
+        double resultadoSubtracao = OperacoesMatematicas.subtracao(1, 4);
+        System.out.println(resultadoSubtracao);
+
+        double resultadoMultiplicacao = OperacoesMatematicas.multiplicacao(2, 4);
+        System.out.println(resultadoMultiplicacao);
+
+        double resultadoDivisao = OperacoesMatematicas.divisao(7, 2);
+        System.out.println(resultadoDivisao);
+
+        int resultadoPotenciacao = OperacoesMatematicas.potenciacao(2, 3);
+        System.out.println(resultadoPotenciacao);
+
     }
 }
